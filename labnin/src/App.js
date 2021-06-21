9177b681512097f20d77733b0ec4c6900adfbf4f
matheuspimentel1795 <matheuspimentel1795@gmail.com>
2021-06-21 16:36:05 -0300

    projeto-dia1

---
+++ labnin/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <div>
+        <h1>Labeninjas</h1>
+        <button>Quero me cadastrar</button>
+        </div>
+        <div>
+        <h1>Conectando quem precisa com quem sabe fazer</h1>
+        <img src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F2a51ad96-d8b7-4df3-bf68-828d473c84ef%2Flabeninjas2.png?table=block&id=5269d1e7-6031-402f-b326-fa0ed68bc43f&spaceId=f97190af-c9c2-4592-9ae2-6311b6b728de&width=740&userId=ad5ed72e-da77-403d-b1c4-6bbb86da6e67&cache=v2"/>
+        </div>
+      </div>
+    )
+  }
+}
